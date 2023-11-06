6ceff73068cb169e6fb54fb95b5e8b46310cfe11
anikdev711 <anikdev100@gmail.com>
2023-11-06 22:53:22 +0600

    create assginment and create user added to database

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 
 
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config()
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
+
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+
 
 
 
@@ -23,6 +30,51 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    //create collections
+
+    const assignmentCollection = client.db('groupStudy').collection('assignments');
+    const userCollection = client.db('groupStudy').collection('users');
+
+
+
+
+
+
+    //create or post assignments
+    app.post('/api/v1/assignments', async (req, res) => {
+      const assignment = req.body;
+      console.log(assignment);
+      const result = await assignmentCollection.insertOne(assignment);
+      res.send(result);
+    })
+
+
+
+
+
+    //user related 
+
+    //user create
+    app.post('/api/v1/users', async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -50,9 +102,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('Online study group server is running successfully')
-  })
-  
-  app.listen(port, () => {
-    console.log(`Group study is listening on port ${port}`)
-  })
+  res.send('Online study group server is running successfully')
+})
+
+app.listen(port, () => {
+  console.log(`Group study is listening on port ${port}`)
+})
